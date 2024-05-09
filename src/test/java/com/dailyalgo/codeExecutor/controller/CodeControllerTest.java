d72d9c6535fe43ca1cdd83effc1b30c68128e797
kylekim2123 <kyle2123@naver.com>
2024-05-09 21:37:47 +0900

    test: 기존 테스트 코드에 Assert문으로 검증 로직 추가

--- src/test/java/com/dailyalgo/codeExecutor/controller/CodeControllerTest.java
+++ src/test/java/com/dailyalgo/codeExecutor/controller/CodeControllerTest.java
@@ -1,74 +1,98 @@
 package com.dailyalgo.codeExecutor.controller;
 
-import com.dailyalgo.codeExecutor.adapter.controller.CodeController;
-import com.dailyalgo.codeExecutor.application.code.dto.RunCodeRequestDto;
-import com.dailyalgo.codeExecutor.application.code.dto.RunCodeResponseDto;
-import java.util.Objects;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
+
+import com.dailyalgo.codeExecutor.adapter.controller.CodeController;
+import com.dailyalgo.codeExecutor.application.code.dto.RunCodeRequestDto;
 
 @SpringBootTest
-public class CodeControllerTest {
+class CodeControllerTest {
 
 	@Autowired
-	CodeController codeController;
+	private CodeController codeController;
 
 	@Test
-	public void python_run_test() {
+	void python_run_test() {
 		String code = """
-			def solution():
-				return 94
-			""";
+				def solution():
+					return 94
+				""";
 		String language = "python";
-		ResponseEntity<RunCodeResponseDto> response = codeController.runCode(new RunCodeRequestDto("python_run_test",  code, language, ""));
-		System.out.println("Result : " + Objects.requireNonNull(response.getBody()).getResult()); // "Result : 94\n
+
+		RunCodeRequestDto request = new RunCodeRequestDto("python_run_test", code, language, "");
+		String result = codeController.runCode(request)
+				.getBody()
+				.getResult();
+
+		System.out.println("Result : " + result); // "Result : 94\n
+		assertThat(result).isEqualTo("94\n");
 	}
 
 	@Test
-	public void java_run_test() {
+	void java_run_test() {
 		String code = """
-			class Solution {
-				String solution() {
-					return "15";
-				}
-			}""";
+				class Solution {
+					String solution() {
+						return "15";
+					}
+				}""";
 		String language = "java";
-		ResponseEntity<RunCodeResponseDto> response = codeController.runCode(new RunCodeRequestDto("java_run_test", code, language, "String"));
-		System.out.println("Result : " + Objects.requireNonNull(response.getBody()).getResult()); // "Result : 15\n
+
+		RunCodeRequestDto request = new RunCodeRequestDto("java_run_test", code, language, "String");
+		String result = codeController.runCode(request)
+				.getBody()
+				.getResult();
+
+		System.out.println("Result : " + result); // "Result : 15\n
+		assertThat(result).isEqualTo("15\n");
 	}
 
 	@Test
-	public void java_int_run_test() {
+	void java_int_run_test() {
 		String code = """
-			class Solution {
-				int solution() {
-			    	return 25;
-				}
-			}""";
+				class Solution {
+					int solution() {
+				    	return 25;
+					}
+				}""";
 		String language = "java";
-		ResponseEntity<RunCodeResponseDto> response = codeController.runCode(new RunCodeRequestDto("java_int_run_test", code, language, "int"));
-		System.out.println("Result : " + Objects.requireNonNull(response.getBody()).getResult()); // "Result : 25\n
+
+		RunCodeRequestDto request = new RunCodeRequestDto("java_int_run_test", code, language, "int");
+		String result = codeController.runCode(request)
+				.getBody()
+				.getResult();
+
+		System.out.println("Result : " + result); // "Result : 25\n
+		assertThat(result).isEqualTo("25\n");
 	}
 
 	@Test
-	public void java_list_run_test() {
+	void java_list_run_test() {
 		String code = """
-			import java.util.ArrayList;
-			import java.util.List;
-			
-			class Solution {
-				List solution() {
-					List<Integer> list = new ArrayList<>();
-					list.add(5);
-					list.add(10);
-					return list;
-				}
-			}""";
+				import java.util.ArrayList;
+				import java.util.List;
+							
+				class Solution {
+					List solution() {
+						List<Integer> list = new ArrayList<>();
+						list.add(5);
+						list.add(10);
+						return list;
+					}
+				}""";
 		String language = "java";
-		ResponseEntity<RunCodeResponseDto> response = codeController.runCode(new RunCodeRequestDto("java_list_run_test", code, language, "List"));
-		System.out.println("Result : " + Objects.requireNonNull(response.getBody()).getResult()); // "Result : 15\n
+
+		RunCodeRequestDto request = new RunCodeRequestDto("java_list_run_test", code, language, "List");
+		String result = codeController.runCode(request)
+				.getBody()
+				.getResult();
+
+		System.out.println("Result : " + result); // "Result : [5, 10]\n
+		assertThat(result).isEqualTo("[5, 10]\n");
 	}
 
 }
