d4cfd08135a8952ef804fce2faa297a719764122
kylekim2123 <kyle2123@naver.com>
2024-05-10 16:21:54 +0900

    refactor: ScoreCodeResponseDto의 생성을 빌더패턴을 통해 할 수 있도록 변경

--- src/main/java/com/dailyalgo/codeExecutor/application/code/dto/ScoreCodeResponseDto.java
+++ src/main/java/com/dailyalgo/codeExecutor/application/code/dto/ScoreCodeResponseDto.java
@@ -1,9 +1,12 @@
 package com.dailyalgo.codeExecutor.application.code.dto;
 
 import java.util.List;
+
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class ScoreCodeResponseDto {
 
 	private final String id;
@@ -11,19 +14,8 @@ public class ScoreCodeResponseDto {
 	private String message;
 	private final List<Boolean> resultList;
 
-	public ScoreCodeResponseDto(String id, List<Boolean> resultList) {
-		this.id = id;
-		this.resultList = resultList;
-	}
-
 	public void addResult(Boolean result) {
 		this.resultList.add(result);
-	}
-
-	public ScoreCodeResponseDto setResult(boolean isCorrect, String message) {
-		this.isCorrect = isCorrect;
-		this.message = message;
-		return this;
 	}
 
 }
